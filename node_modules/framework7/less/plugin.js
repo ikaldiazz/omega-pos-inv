8293363ea8909f17d93705c54aac545d321d95c7
ikaldiazz <ikaldiaz@gmail.com>
2018-12-11 23:53:50 +0700

    updating config

--- node_modules/framework7/less/plugin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  install(less, pluginManager, functions) {
-    functions.add('framework7_encodeURIComponent', svg => encodeURIComponent(svg.value));
-    functions.add('framework7_floor', n => new less.tree.Dimension(Math.floor(n.value)));
-    functions.add('framework7_calculateN', n => new less.tree.Dimension(100 / parseFloat(n.value)));
-  },
-};
